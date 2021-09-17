b2c5f2aa15e2f30be33138409dbd9ed39aac8202
rikichirinkato <roman28krasko@gmail.com>
2021-09-17 11:58:12 +0300

    rewrited test

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,17 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import App from './App';
 import { createStore } from 'redux';
 import reducer from './reducer';
 
+let store;
+
+beforeEach(() => {
+    store = createStore(reducer);
+    render(<Provider store={store}><App /></Provider>);
+});
 
 test('renders header with', () => {
-  const store = createStore(reducer);
-  render(<Provider store={store}><App /></Provider>);
-  const h1 = screen.queryByText('Approved images (0)');
-  expect(h1).toBeTruthy();
+    const h1 = screen.queryByText('Approved images (0)');
+    expect(h1).toBeInTheDocument();
 });
