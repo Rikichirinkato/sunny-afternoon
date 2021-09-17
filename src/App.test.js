162c1a9fa513503a82ab3d2594d1acf0a10a88a6
rikichirinkato <roman28krasko@gmail.com>
2021-09-17 16:50:19 +0300

    added tests for RandomImage

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,15 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import App from './App';
 import { createStore } from 'redux';
 import reducer from './reducer';
 
-let store;
-
-beforeEach(() => {
-    store = createStore(reducer);
-    render(<Provider store={store}><App /></Provider>);
-});
-
-test('renders header with', () => {
-    const h1 = screen.queryByText('Approved images (0)');
-    expect(h1).toBeInTheDocument();
+describe('it shoud render component', () => {
+    it('renders app with empty state', () => {
+        const store = createStore(reducer);
+        render(<Provider store={store}><App /></Provider>);
+        const h1 = screen.queryByText('Approved images (0)');
+        expect(h1).toBeInTheDocument();
+        expect(screen.getByTestId('empty-state')).toBeInTheDocument();
+    });    
 });
